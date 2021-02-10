d768dd59453f6d7e2d20e0b6a5020f1d27a443bf
Leonel Parrales <dparrales@est.ups.edu.ec>
2021-02-09 22:10:40 -0500

    Como usar activity 1

---
+++ app/src/main/java/com/ksld/appemergencia/ComoFunciona_1.java
@@ -0,0 +1,14 @@
+package com.ksld.appemergencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComoFunciona_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_como_funciona_1);
+    }
+}
